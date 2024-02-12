661e25560dfff6855ce0efb8143c2f7a5f98aa86
Hemanth Lokesh Kumar Lanka <lankahemanthlokeshkumar@gmail.com>
2024-02-12 21:36:28 +0530

    adding conditions program

---
+++ conditions.kt
@@ -0,0 +1,35 @@
+fun main()
+{
+    val weekday="Friday"
+    println("On $weekday, the opening hours are:")
+    if(weekday=="Monday")
+    {
+        println("8 AM to 12 PM")
+    }
+    else if(weekday=="Tuesday")
+    {
+        println("8 AM to 6 PM")
+    }
+    else if(weekday=="Wednesday")
+    {
+        println("8 AM to 6 PM")
+    }
+    else if(weekday=="Thursday")
+    {
+        println("8 AM to 6 PM")
+    }
+    else if(weekday=="Friday")
+    {
+        println("8 AM to 9 PM")
+    }
+    else if(weekday=="Saturday")
+    {
+        println("9 AM to 4 PM")
+    }
+    else if(weekday=="Sunday")
+    {
+        println("8 AM to 4 PM")
+    }
+
+
+}
