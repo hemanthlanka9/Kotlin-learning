d00fe966ac1b439d5eb9fc5589df8f45cb370e89
Hemanth Lokesh Kumar Lanka <lankahemanthlokeshkumar@gmail.com>
2024-02-12 21:42:50 +0530

    Adding when code to main

---
+++ when.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    val weekday="Friday"
+    when(weekday)
+    {
+        "Monday"->println("8 AM to 12 PM")
+        "Tuesday"->println("8 AM to 6 PM")
+        "Wednesday"->println("8 AM to 6 PM")
+        "Thurssday"->println("8 AM to 6 PM")
+        "Friday"->println("8 AM to 6 PM")
+        "Saturday"->println("8 AM to 6 PM")
+        "Sunday"->println("8 AM to 6 PM")
+    }
+}
